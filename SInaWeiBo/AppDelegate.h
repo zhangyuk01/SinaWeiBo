1aaff0f04eaa37898fd5a5e1f183f7dcb239a81d
Zhang Yu <zhangyu@happysoft.cc>
2015-11-15 22:26:53 +0800

    init

---
+++ SInaWeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SInaWeiBo
+//
+//  Created by 张瑜 on 15/11/15.
+//  Copyright (c) 2015年 张瑜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SInaWeiBo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SInaWeiBo
+//
+//  Created by 张瑜 on 15/11/15.
+//  Copyright (c) 2015年 张瑜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
