04d02fcbf93d76fe3535fd5c9fd91a8a1bd1010a
zhangyu <zhangyukjn@sina.com>
2015-11-17 15:21:18 +0800

    添加自定义tabbar

--- SInaWeiBo/ZYTabBar.m
+++ SInaWeiBo/ZYTabBar.m
@@ -8,7 +8,50 @@
 
 #import "ZYTabBar.h"
 
+@interface ZYTabBar()
+
+// jiahao
+@property(nonatomic, weak) UIButton *plusBtn;
+
+@end
+
+
 @implementation ZYTabBar
+
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setUp];
+    }
+    return  self;
+}
+
+
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setUp];
+    }
+    return self;
+}
+
+
+- (void) setUp
+{
+    UIButton *plusBtn = [[UIButton alloc] init];
+    [plusBtn setBackgroundImage:[UIImage imageNamed:@"tabbar_compose_button"] forState:UIControlStateNormal];
+    [plusBtn setBackgroundImage:[UIImage imageNamed:@"tabbar_compose_button_highlighted"] forState:UIControlEventTouchUpInside];
+    [plusBtn setImage:[UIImage imageNamed:@"tabbar_compose_icon_add"] forState:UIControlStateNormal];
+    [plusBtn setImage:[UIImage imageNamed:@"tabbar_compose_icon_add_highlighted"] forState:UIControlStateHighlighted];
+    [self addSubview:plusBtn];
+    self.plusBtn = plusBtn;
+}
+
+
 
 // 当子布局改变的时候会调用这个方法，也可以直接调用这个方法，来进行改变布局
 - (void)layoutSubviews
@@ -19,7 +62,7 @@
       /** 这个地方要进行判断，为什么呢？因为tabar里面有多个多个button，分割线，还有背景图片
         我们只要对tabbarbutton来进行一些设置 , 根据类型来进行判断
       */
-        if ([child isKindOfClass:[UIControl class]]) {
+        if ([child isKindOfClass:[UIControl class]] && ![child isKindOfClass:[UIButton class]]) {
             CGFloat childW = self.frame.size.width / 5;
             CGFloat childH = self.frame.size.height;
             CGFloat childX = index*childW;
@@ -31,6 +74,12 @@
             child.frame = CGRectMake(childX, childY, childW, childH);
             index ++;
        }
+        
+        self.plusBtn.size = self.plusBtn.currentBackgroundImage.size;
+        self.plusBtn.centerX = self.width*0.5;
+        self.plusBtn.centerY = self.height*0.5;
     }
+   
+    
 }
 @end
