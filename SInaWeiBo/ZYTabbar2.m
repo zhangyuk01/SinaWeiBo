91b32a9cbeb94c9db2a3c7409eea349ae68e009b
zhangyu <zhangyukjn@sina.com>
2015-11-17 18:18:28 +0800

    添加自定义tabbar

---
+++ SInaWeiBo/ZYTabbar2.m
@@ -0,0 +1,73 @@
+//
+//  ZYTabbar2.m
+//  SInaWeiBo
+//
+//  Created by huahua on 11/17/15.
+//  Copyright (c) 2015 张瑜. All rights reserved.
+//
+
+#import "ZYTabbar2.h"
+
+@implementation ZYTabbar2
+
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setUp];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setUp];
+    }
+    return self;
+}
+
+- (void) setUp
+{
+    self.backgroundColor = [UIColor redColor];
+}
+
+
+- (void) addItem:(UITabBarItem *)item
+{
+    
+    // 创建选项卡按钮
+    UIButton *btn = [[UIButton alloc] init];
+   
+    [btn setTitle:item.title forState:UIControlStateNormal];
+    [btn  setImage:item.image forState:UIControlStateNormal];
+    [btn setImage:item.selectedImage forState:UIControlStateSelected];
+    
+    [self addSubview:btn];
+}
+
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    NSUInteger index = 0;
+    for (UIView *child in self.subviews) {
+        
+            CGFloat childW = self.frame.size.width / 5;
+            CGFloat childH = self.frame.size.height;
+            CGFloat childX = index*childW;
+            CGFloat childY = 0;
+            if (index == 2) {
+                index ++;
+                childX = index*childW;
+            }
+            index ++;
+            child.frame = CGRectMake(childX, childY, childW, childH);
+        
+        }
+
+}
+
+@end
